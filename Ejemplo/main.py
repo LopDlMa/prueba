046fb2038051b244507990c7502bd210a6cf3136
LopDlMa <dulcita11@gmail.com>
2022-03-02 19:13:15 -0600

    triangulo

--- Ejemplo/main.py
+++ Ejemplo/main.py
@@ -1,1 +1,13 @@
+import turtle
 print("Hello World")
+
+tortuga = turtle.Turtle()
+
+tortuga.left(45)
+tortuga.forward(50)
+tortuga.right(90)
+tortuga.forward(50)
+tortuga.right(135)
+tortuga.forward(75)
+
+turtle.done()
